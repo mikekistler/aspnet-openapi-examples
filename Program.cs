afcc83550b189cbf0bd75c37e696fbdb36884011
Mike Kistler (from Dev Box) <mikekistler@microsoft.com>
2024-06-01 13:48:38 -0700

    Add servers

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 
 builder.Services.AddOpenApi(options => {
     options.UseTransformer<InfoTransformer>();
+    options.UseTransformer((document, context, cancellationToken) =>
+    {
+        document.Servers = new List<OpenApiServer>
+        {
+            new OpenApiServer
+            {
+                Url = "http://localhost:5000",
+                Description = "Local development server"
+            }
+        };
+        return Task.CompletedTask;
+    });
 });
 
 var app = builder.Build();
