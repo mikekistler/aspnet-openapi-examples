d79957fb2a9c0c5c5dbeb17fa1d4b7eb7d7f5a8b
Mike Kistler <mikekistler@microsoft.com>
2024-11-04 13:48:57 -0800

    Add project and notebook for ErrorHandling

--- ErrorHandling/Program.cs
+++ ErrorHandling/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Minimal API apps can be configured to generate problem details response
+// for all HTTP client and server error responses that don't have body content
+// yet by using the AddProblemDetails extension method.
+// ref: https://learn.microsoft.com/aspnet/core/fundamentals/minimal-apis/handle-errors#problem-details
+builder.Services.AddProblemDetails();
+
+// ProblemDetailsServiceCollectionExtensions.AddProblemDetails(builder.Services);
 
 var app = builder.Build();
 
@@ -12,30 +23,24 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// The Status Code Pages middleware can be configured to produce a common body content, when empty,
+// for all HTTP client (400-499) or server (500 -599) responses.
+// ref: https://learn.microsoft.com/aspnet/core/fundamentals/minimal-apis/handle-errors#problem-details
+app.UseStatusCodePages();
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/requires-query-param", (int foo) => "Hello World!");
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/requires-path-param/{id}",
+// C# cannot infer the union return type, so we must declare it explicitly
+Results<Ok<string>, NotFound<ProblemDetails>> (int id) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (id < 0 || id > 10)
+    {
+        return TypedResults.NotFound(new ProblemDetails());
+    }
+    return TypedResults.Ok($"Hello World with id={id}!");
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
